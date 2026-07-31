662ddcf15c0d7d3b2107f92150d5c63d7b0198d9
Taku-Shimamoto <ithachioji31.562@gmail.com>
2026-07-31 15:15:16 +0900

    7/31

---
+++ js/works.js
@@ -0,0 +1,3 @@
+const communicationGameAppsButton = document.getElementById("communication_game_apps");
+
+communicationGameAppsButton.addEventListener("click", () => location.href = "communication_games/index.html");
